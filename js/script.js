ab07fdbeb5470689cce7f159059d30e1e9b05ee1
Maria <94925262+Maria-developer@users.noreply.github.com>
2022-06-12 12:25:58 -0400

    Removed logs to console

--- js/script.js
+++ js/script.js
@@ -56,17 +56,14 @@ repoList.addEventListener("click", function(e) {
 async function getRepoInfo (repoName) {
     const fetchRepoInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
     const repoInfo = await fetchRepoInfo.json();
-    console.log(repoInfo);
 
     const fetchLanguages = await fetch(repoInfo.languages_url);
     const languageData = await fetchLanguages.json();
-    console.log(languageData);
 
     const languages = [];
     for (const language in languageData) {
         languages.push(language);
     }
-    console.log(languages);
 
     displayRepoInfo(repoInfo, languages);
 };
